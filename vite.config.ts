900b94403cfde2d13d7af5c0159884d6af690c33
James Giles IV <james@opsvio.com>
2025-03-08 22:38:21 -0600

    updating files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://opsvio.github.io/', // Replace with your repository name
   server: {
     port: 5173,
     host: true
